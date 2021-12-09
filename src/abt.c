b44c2b51b9f70ea56bbe518111665a5b581b41ca
Rajesh Reddy Mekala <mekalarajeshreddy@gmail.com>
2021-12-09 16:52:12 -0500

    updated abt

--- src/abt.c
+++ src/abt.c
@@ -1,11 +1,11 @@
 #include "../include/simulator.h"
 #include<stdio.h>
-
-
+ 
+ 
 /* ******************************************************************
  ALTERNATING BIT AND GO-BACK-N NETWORK EMULATOR: VERSION 1.1  J.F.Kurose
-
-   This code should be used for PA2, unidirectional data transfer 
+ 
+   This code should be used for PA2, unidirectional data transfer
    protocols (from A to B). Network properties:
    - one way network delay averages five time units (longer if there
      are other messages in the channel for GBN), but can be larger
@@ -14,20 +14,20 @@
    - packets will be delivered in the order in which they were sent
      (although some can be lost).
 **********************************************************************/
-
+ 
 /********* STUDENTS WRITE THE NEXT SIX ROUTINES *********/
-
+ 
 #define A 0
 #define B 1
-
-int a_seqnum, b_seqnum;
+ 
+int a_seqnum, b_seqnum, last_seqnum, last_acknum;
 int a_state;  // value would be 0 (waiting for ACK) or 1 (waiting for LAYER5)
 struct pkt last_packet; //used to send last packet again if no ack is received
-
+ 
 int checksum(struct pkt packet){
 	int checksum=0, i;
 	for(i=0;i<20;i++){
-	    if(packet.payload[i]='\0') {
+	    if(packet.payload[i]=='\0') {
 	    	break;
 		}
 		checksum+=packet.payload[i];
@@ -36,7 +36,7 @@ int checksum(struct pkt packet){
 	checksum+=packet.acknum;
 	return checksum;
 }
-
+ 
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(struct msg message)
 {
@@ -44,45 +44,51 @@ void A_output(struct msg message)
 	 then buffer the message and send later*/
 	if(a_state == 0 ) {
 		return;
-	} 
+	}
 	int i;
 	struct pkt packet;
-	
-    for(i=0;i<20;i++) {	    
-		if(message.data[i]='\0') {
+ 
+    for(i=0;i<20;i++) {
+		if(message.data[i]=='\0') {
 	    	break;
 		}
-    	
+ 
     	packet.payload[i] = message.data[i];
 	}
+	packet.payload[i]='\0';
+ 
 	packet.seqnum = a_seqnum;
 	packet.checksum = checksum(packet);
 	last_packet = packet;
-    printf("sending message to B\n");
-	a_state = 0; 	
+   printf("sending message to B\n");
+	a_state = 0;
 	starttimer(A, 25.0);
 	tolayer3(A, packet);
 }
-
+ 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(struct pkt packet)
 {
-	if(a_seqnum != packet.acknum) {
+	if(a_seqnum != packet.acknum && last_acknum != packet.acknum) {
 		starttimer(A, 25.0);
 		tolayer3(A, last_packet);
+	} else if(last_acknum == packet.acknum) {
+		// do nothing
+	} else {
+		stoptimer(A);
+		last_acknum = a_seqnum;
+		a_seqnum = 1-a_seqnum;
+		a_state=1;
 	}
-	stoptimer(A);
-	a_seqnum = 1-a_seqnum;
-	a_state=1;
 }
-
+ 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
 	starttimer(A, 25.0);
 	tolayer3(A, last_packet);
-}  
-
+}
+ 
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
 void A_init()
@@ -90,35 +96,36 @@ void A_init()
 	a_seqnum = 0;
 	a_state = 1;
 }
-
+ 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
-
+ 
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet)
 {
 	struct pkt ack_packet;
 	if(packet.checksum != checksum(packet)) {
 		//packet is corrupted
-		ack_packet.acknum = 1-b_seqnum;
+		ack_packet.acknum = -1;
 		tolayer3(B, ack_packet);
 		return;
-	}
-    else if(packet.seqnum != b_seqnum){
-		//we received unexpected packet
+	} else if (packet.seqnum != b_seqnum && last_seqnum == packet.seqnum) {
+		// ack lost
 		ack_packet.acknum = 1-b_seqnum;
 		tolayer3(B, ack_packet);
 		return;
 	}
 	tolayer5(B, packet.payload);
 	ack_packet.acknum = b_seqnum;
+	last_seqnum = packet.seqnum;
 	tolayer3(B, ack_packet);
-	b_seqnum = 1-b_seqnum;	
+	b_seqnum = 1-b_seqnum;
 }
-
+ 
 /* the following routine will be called once (only) before any other */
 /* entity B routines are called. You can use it to do any initialization */
 void B_init()
 {
 	b_seqnum = 0;
+	last_seqnum = -1;
 }
-
+ 
